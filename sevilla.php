22bb850799f85e0914e7e35f309016f815a6fa98
patxisantos <patxisantosrivas@gmail.com>
2017-12-25 21:38:50 +0100

    Desprotegidos php del login
    Desprotección del login en los documentos necesarios.

--- sevilla.php
+++ sevilla.php
@@ -3,8 +3,8 @@ require_once("./include/membersite_config.php");
 
 if(!$fgmembersite->CheckLogin())
 {
-    $fgmembersite->RedirectToURL("login.php");
-    exit;
+    //$fgmembersite->RedirectToURL("login.php");
+    //exit;
 }
 
 ?>
